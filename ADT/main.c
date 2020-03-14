810344d849687c696389542d2e471b3169884fcc
tslothorst <tslothorst@gmail.com>
2020-03-14 16:53:42 +0100

    Added max and min functions

--- ADT/main.c
+++ ADT/main.c
@@ -77,6 +77,26 @@ void Set(struct Array* arr, int index, int x){
     }
 }
 
+int Max(struct Array arr){
+    int max=arr.A[0];
+    for (int i = 1; i < arr.length; ++i) {
+        if(arr.A[i]>max){
+            max=arr.A[i];
+        }
+    }
+    return max;
+}
+
+int Min(struct Array arr){
+    int min=arr.A[0];
+    for (int i = 1; i < arr.length; ++i) {
+        if(arr.A[i]<min){
+            min = arr.A[i];
+        }
+    }
+    return min;
+}
+
 int main() {
     struct Array arr1 = {{2,23,14,5,6,9,8,12},10,8};
     Display(arr1);
