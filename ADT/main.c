a15e94d7a47786e714cf785565925fe44151201c
tslothorst <tslothorst@gmail.com>
2020-03-22 16:15:14 +0100

    Added merge function

--- ADT/main.c
+++ ADT/main.c
@@ -159,6 +159,33 @@ void Rearrange(struct Array* arr){
     }
 }
 
+struct Array* Merge(struct Array* arr1, struct Array* arr2){
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    struct Array* arr3=(struct Array*)malloc(sizeof(struct Array));
+
+    while(i<arr1->length && j<arr2->length){
+        if(arr1->A[i]<arr2->A[j]){
+            arr3->A[k++]=arr1->A[i++];
+        }
+        else{
+            arr3->A[k++]=arr2->A[j++];
+        }
+    }
+    for(;i<arr1->length;i++){
+        arr3->A[k++]=arr1->A[i];
+    }
+    for(;j<arr2->length;j++){
+        arr3->A[k++]=arr2->A[j];
+    }
+    arr3->length=arr1->length+arr2->length;
+    arr3->size=10;
+
+    return arr3;
+}
+
 int main() {
     struct Array arr1 = {{2,23,14,5,6,9,8,12},10,8};
     Display(arr1);
